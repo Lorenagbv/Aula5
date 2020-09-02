ff0e3c3141a53b3d6568c9b3a36eded99e867a31
Carol <lorena.vianna8@gmail.com>
2020-09-01 23:00:47 -0300

    aula5 envio

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿const express = require('express')
+const app = express()
+const valorModel = require('./models/valorModel')
+const jwt = require('jsonwebtoken')
+const body = require('body-parser')
+const {urlencoded} = require('body-parser')
+
+app.get('/alunos/todos', (req, res) => {
+	let alunos = [
+		{'nome': 'Lorena', 'area': 'Sistemas'},
+		{'nome': 'Caroline', 'area': 'Sistemas'},
+		{'nome': 'Gabriel', 'area': 'Zootecnia'},
+	]
+	res.json(alunos)
+})
+
+app.listen(3000, () => {
+	console.log('App rodando!')
+})
